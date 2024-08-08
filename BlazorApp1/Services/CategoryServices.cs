845bd4365ea79325b5b4e56d4f54abe7d4eeeede
jeffersondavila <jeffersondavila16@gmail.com>
2024-08-07 21:14:12 -0600

    [ADD] Se agrega el servicio de categoria

--- BlazorApp1/Services/CategoryServices.cs
+++ BlazorApp1/Services/CategoryServices.cs
@@ -1,6 +1,31 @@
-﻿namespace BlazorApp1.Services
+﻿using BlazorApp1.Models;
+using System.Text.Json;
+
+namespace BlazorApp1.Services
 {
 	public class CategoryServices
 	{
+		private readonly HttpClient client;
+		private readonly JsonSerializerOptions options;
+
+		public CategoryServices(HttpClient httpClient)
+		{
+			this.client = httpClient;
+			options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+		}
+
+		public async Task<List<Category>?> GetCategorias()
+		{
+			var response = await client.GetAsync("/v1/categories");
+			var content = await response.Content.ReadAsStringAsync();
+
+			if(!response.IsSuccessStatusCode)
+			{
+				throw new ApplicationException(content);
+			}
+
+			// return await JsonSerializer.DeserializeAsync<List<Category>>(await response.Content.ReadAsStreamAsync());
+			return JsonSerializer.Deserialize<List<Category>>(content,options);
+		}
 	}
 }
